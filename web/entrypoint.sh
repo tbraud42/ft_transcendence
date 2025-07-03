fd783028ed7307a1585e423d8190a08d08374f64
CleSucre <julien.thomas.4321@gmail.com>
2025-07-03 03:40:08 +0200

    fix: Refactor Docker and Nginx setup for web and nginx services
    web service is now an nginx server to simplifi deployment in production

--- web/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-npm run watch & # Start the watch process in the background
-
-#copy all assets to the public directory excluding all .ts files
-rsync -a --exclude='*.ts' --exclude='*.js' --exclude='*.map' --exclude='.git' ./src/ ./dist/
-
-npm run copy-assets # Sync assets to the public directory
