e3c91fc2d44f6eb10f1b4c256136196e54b2378a
CleSucre <julien.thomas.4321@gmail.com>
2025-10-21 15:53:05 +0200

    fix: env variable in production mode not loaded

--- web/src/utils/env.ts
+++ web/src/utils/env.ts
@@ -1,5 +1,6 @@
 export const env = {
-    API_URL: "https://" + import.meta.env.VITE_API_URL,
     DOMAIN: "https://" + import.meta.env.VITE_DOMAIN,
+    API_URL: "https://" + import.meta.env.VITE_API_URL,
     PONG_WS_URL: "https://" + import.meta.env.VITE_PONG_WS_URL,
+    NODE_ENV: import.meta.env.VITE_NODE_ENV,
 } as const;
