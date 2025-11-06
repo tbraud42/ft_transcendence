a9258512413ab7c99c72f98d34565e531183cb58
CleSucre <julien.thomas.4321@gmail.com>
2025-11-06 15:49:21 +0100

    fix: avatar limit overlay fixed

--- web/src/utils/files.ts
+++ web/src/utils/files.ts
@@ -6,3 +6,13 @@ export function fileToBase64(file: File): Promise<string> {
         reader.readAsDataURL(file)
     })
 }
+
+export function byteSizeToHumanReadable(size: number): string {
+    if (size === 0) return '0 B';
+
+    const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB'];
+    const i = Math.floor(Math.log(size) / Math.log(1024));
+    const value = size / Math.pow(1024, i);
+
+    return `${value.toFixed(2)} ${units[i]}`;
+}
