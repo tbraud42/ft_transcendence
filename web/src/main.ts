9f3724ed68fd64c62b1f2bc2ace938c0d09956ca
CleSucre <julien.thomas.4321@gmail.com>
2025-11-10 15:10:35 +0100

    Revert "fix: remove dark"
    This reverts commit be1099903f8c315650fdef7e1fefdd8f6a42ef48.

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,6 +9,8 @@ link.type = 'image/webp'
 link.href = faviconUrl
 document.head.appendChild(link)
 
+document.documentElement.classList.add('dark')
+
 window.addEventListener('DOMContentLoaded', router)
 window.addEventListener('hashchange', router)
 window.addEventListener('popstate', () => {
