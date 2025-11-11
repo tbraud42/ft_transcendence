18ffc0910a703aa87ab5ba8c16ba6351f4d58086
CleSucre <julien.thomas.4321@gmail.com>
2025-11-11 06:54:38 +0100

    fix: tournament state & Makefile

--- Makefile
+++ Makefile
@@ -10,22 +10,22 @@ COMPOSE_FILE = docker-compose.yml
 DEV_OVERRIDE = docker-compose.override.yml
 
 up:
-	docker-compose -f $(COMPOSE_FILE) up --build -d
+	docker compose -f $(COMPOSE_FILE) up --build -d
 
 build:
-	docker-compose -f $(COMPOSE_FILE) build --no-cache
+	docker compose -f $(COMPOSE_FILE) build --no-cache
 
 logs:
-	docker-compose -f $(COMPOSE_FILE) logs -f
+	docker compose -f $(COMPOSE_FILE) logs -f
 
 dev:
-	docker-compose -f $(COMPOSE_FILE) -f $(DEV_OVERRIDE) up --build
+	docker compose -f $(COMPOSE_FILE) -f $(DEV_OVERRIDE) up --build
 
 down:
-	docker-compose -f $(COMPOSE_FILE) down
+	docker compose -f $(COMPOSE_FILE) down
 
 clean:
-	docker-compose -f $(COMPOSE_FILE) down --volumes --remove-orphans
+	docker compose -f $(COMPOSE_FILE) down --volumes --remove-orphans
 	$(RM) .$(DIRSEP)nginx$(DIRSEP)data
 	$(RM) .$(DIRSEP)web$(DIRSEP)dist
 	$(RM) .$(DIRSEP)sqlite_api$(DIRSEP)data
--- ws_server/src/core/game/Tournament.js
+++ ws_server/src/core/game/Tournament.js
@@ -37,7 +37,7 @@ export class Tournament {
      */
     addPlayer(client) {
         const username = client.getUsername();
-        if (!username || this.participants.has(username) || this.participants.size >= this.maxPlayers || this.isEnded()) {
+        if (!username || this.participants.has(username) || this.isFull() || this.isEnded()) {
             return false;
         }
 
@@ -60,7 +60,7 @@ export class Tournament {
      * No-op if tournament is full or room/slot is invalid.
      */
     addBotAt(roomId, slot) {
-        if (this.participants.size >= this.maxPlayers) {
+        if (this.isFull() || this.isEnded()) {
             return;
         }
 
@@ -102,7 +102,7 @@ export class Tournament {
             return;
         }
 
-        if (this.isFull()) {
+        if (this.isFull() && !this.isEnded()) {
             updateTournamentState(this.creator, this.id, 0).then();
         }
 
